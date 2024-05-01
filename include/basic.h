f254ae9beb6879c7b7cb7dfdd3a6654cf1ba02a9
Sayan Shankhari <sayan0shankhari@gmail.com>
2024-05-01 10:55:23 +0530

    feat: add custom library
    Able to create final executable file via custom library headers.
    
    Added custom library header.

---
+++ include/basic.h
@@ -0,0 +1,11 @@
+#ifndef BASIC_H
+#define BASIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+#endif // BASIC_H
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "./include/basic.h"
 
 int main (void) {
 	printf ("Hello, World!\n");

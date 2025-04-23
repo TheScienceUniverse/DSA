04d4aa3f58f64496b7037e74edd876ed95c806aa
Sayan Shankhari <sayan0shankhari@gmail.com>
2025-04-23 11:07:48 +0530

    feat (functionality): push and pop Data with Stack
    Able to push Data onto Stack and pop Data from Stack.
    
    Create push and pop functionality to insert or remove Data
    similar to Node. Returned data to be deleted manually.
    Add tests to confirm the functionalities.
    
    Resolves: #87

--- include/stack.h
+++ include/stack.h
@@ -22,5 +22,7 @@ void display_stack_details (Stack* stack);
 void push_node_onto_stack (Stack* stack, Node* node);
 Node* pop_node_from_stack (Stack* stack);
 void peek_data_from_stack (Stack* stack);
+void push_data_onto_stack (Stack* stack, Data* data);
+Data* pop_data_from_stack (Stack* stack);
 
 #endif	// STACK_H

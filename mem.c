cfedec9ec0b764b2230de0488916620c91af51bd
Sayan Shankhari <sayan0shankhari@gmail.com>
2025-05-23 09:46:20 +0530

    fix (logging): fix memory logger
    Able to compile and generate memory logger program.
    
    Change minimum allowed argument count to 2, as the first
    argument is the program itself.
    Change exit function statement with return keyword statement.
    
    Resolves: #98

--- mem.c
+++ mem.c
@@ -2,9 +2,9 @@
 #include "./include/string.h"
 
 int main (int argc, char* argv[]) {
-	if (argc < 1) {
+	if (argc < 2) {
 		perror ("No option is given!");
-		exit EXIT_FAILURE;
+		return EXIT_FAILURE;
 	}
 
 	if (compare_raw_strings_shh (4, argv [1], 4, "show")) {
